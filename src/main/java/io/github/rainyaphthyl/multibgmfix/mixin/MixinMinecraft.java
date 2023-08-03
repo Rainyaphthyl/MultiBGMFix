ba26507b210acc046182f611aa6d03d550b8ab19
Natfalluvia <amena_rny@outlook.com>
2023-08-03 23:14:51 +0800

    Fix mixin fatal

--- src/main/java/io/github/rainyaphthyl/multibgmfix/mixin/MixinMinecraft.java
+++ src/main/java/io/github/rainyaphthyl/multibgmfix/mixin/MixinMinecraft.java
@@ -6,7 +6,6 @@ import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.entity.player.PlayerCapabilities;
 import org.spongepowered.asm.lib.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
@@ -14,9 +13,6 @@ import javax.annotation.Nonnull;
 
 @Mixin(Minecraft.class)
 public abstract class MixinMinecraft {
-    @Shadow
-    public EntityPlayerSP player;
-
     /**
      * Allows permitted spectators to play creative music.
      *
@@ -30,7 +26,11 @@ public abstract class MixinMinecraft {
             if (capabilities.isCreativeMode) {
                 flag = true;
             } else {
-                flag = capabilities.disableDamage && player.canUseCommand(2, "");
+                flag = capabilities.disableDamage;
+                if (flag) {
+                    EntityPlayerSP player = Minecraft.getMinecraft().player;
+                    flag = player.canUseCommand(2, "");
+                }
             }
             return flag;
         } else {
