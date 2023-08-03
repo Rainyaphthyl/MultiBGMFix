2a8a7e9e0e5ca43d85d26984552873105d8c2354
Natfalluvia <amena_rny@outlook.com>
2023-08-03 23:16:27 +0800

    Version `0.1.2`

--- src/main/java/io/github/rainyaphthyl/multibgmfix/LiteModMultiBGMFix.java
+++ src/main/java/io/github/rainyaphthyl/multibgmfix/LiteModMultiBGMFix.java
@@ -14,7 +14,7 @@ import java.io.File;
 @SuppressWarnings("unused")
 public class LiteModMultiBGMFix implements Configurable, InitCompleteListener {
     public static final String NAME = "Multi BGM Fix";
-    public static final String VERSION = "0.1.2-alpha.0";
+    public static final String VERSION = "0.1.2";
     public static final ModVersion versionObj = ModVersion.getVersion(VERSION);
 
     public ModSettings getSettings() {
