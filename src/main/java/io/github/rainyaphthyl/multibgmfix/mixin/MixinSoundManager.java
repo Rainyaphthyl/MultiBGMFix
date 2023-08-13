0fd84fe217c5f5102ccea983951296f4e1992655
Natfalluvia <amena_rny@outlook.com>
2023-08-13 22:08:31 +0800

    Fix debug to build 1

--- src/main/java/io/github/rainyaphthyl/multibgmfix/mixin/MixinSoundManager.java
+++ src/main/java/io/github/rainyaphthyl/multibgmfix/mixin/MixinSoundManager.java
@@ -4,11 +4,14 @@ import io.github.rainyaphthyl.multibgmfix.util.GenericHelper;
 import net.minecraft.client.audio.ISound;
 import net.minecraft.client.audio.Sound;
 import net.minecraft.client.audio.SoundManager;
+import org.spongepowered.asm.lib.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import javax.annotation.Nonnull;
@@ -19,11 +22,26 @@ public abstract class MixinSoundManager {
     @Shadow
     @Final
     private Map<ISound, String> invPlayingSounds;
+    @Unique
+    private boolean multiBGMFix$currStreaming = false;
 
-    @Inject(method = "playSound", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/audio/SoundManager$SoundSystemStarterThread;newStreamingSource(ZLjava/lang/String;Ljava/net/URL;Ljava/lang/String;ZFFFIF)V"))
-    public void debugMusicPlaying(@Nonnull ISound p_sound, CallbackInfo ci) {
-        Sound sound = p_sound.getSound();
-        String sourceKey = invPlayingSounds.get(p_sound);
-        GenericHelper.LOGGER.info("Add source '{}' -> '{}'", sourceKey, sound.getSoundAsOggLocation());
+    @Redirect(method = "playSound", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/audio/Sound;isStreaming()Z"))
+    public boolean markSoundStreaming(@Nonnull Sound sound) {
+        if (sound.isStreaming()) {
+            multiBGMFix$currStreaming = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Inject(method = "playSound", at = @At(value = "FIELD", target = "Lnet/minecraft/client/audio/SoundManager;categorySounds:Lcom/google/common/collect/Multimap;", opcode = Opcodes.GETFIELD))
+    public void debugStreamingSound(@Nonnull ISound p_sound, CallbackInfo ci) {
+        if (multiBGMFix$currStreaming) {
+            multiBGMFix$currStreaming = false;
+            Sound sound = p_sound.getSound();
+            String sourceKey = invPlayingSounds.get(p_sound);
+            GenericHelper.LOGGER.info("Add source '{}' -> '{}'", sourceKey, sound.getSoundAsOggLocation());
+        }
     }
 }
